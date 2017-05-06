f25dafed658739464f55fd8b2e99dfece7e4d232
aripal <dleroari@gmail.com>
2017-05-05 21:18:32 -0400

    Added more funcitonality to time_series.py

--- time_series.py
+++ time_series.py
@@ -11,18 +11,27 @@ from matplotlib.dates import DateFormatter
 # Read from the CSV file
 data = read_csv('GoogleCloudTest.csv')
 
+xnew = np.linspace(0, 10, num=41, endpoint=True)
+
 # Get Data from CSV file
 t = data['Date / Time']
 x = data['CPU Usage %']
 y = data['Temperature C']
 
 # Labels 
+# plt.xlabel(temper)
+
+# Plot colorbar
+# plt.colorbar() 
 
 
 #Plot data into diagram
 plt.plot_date(t,x, 'b-')
 plt.plot_date(t,y, 'g-')
 plt.gcf().autofmt_xdate()
+
+#Title 
+plt.title('GodMode: Time Series')
 plt.show()
 
 
