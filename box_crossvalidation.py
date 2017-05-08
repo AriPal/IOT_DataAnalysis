1c6b9ffc9abca62539f0801626e08cfe69ff73a6
aripal <dleroari@gmail.com>
2017-05-08 14:26:51 -0400

    It works!

--- box_crossvalidation.py
+++ box_crossvalidation.py
@@ -1,13 +1,9 @@
 #Include library
-import numpy as np 
-from scipy import stats 
-import matplotlib.pyplot as plt 
-import pdb
 from pandas import *
-import datetime
 from sklearn import datasets
 from sklearn.model_selection import cross_val_predict
 from sklearn import linear_model
+import matplotlib.pyplot as plt
 
 
 # Read from the CSV file
@@ -16,19 +12,35 @@ data = read_csv('rawdata.csv')
 # Get Data from CSV file
 t = data['Date / Time']
 x = data['CPU Usage %']
-y = data['Temperature C']
+data = data['Temperature C']
 
-lr = linear_model.LinearRegression()
-boston = datasets.load_boston()
-y = boston.target
+# lr = linear_model.LinearRegression()
+# boston = datasets.load_boston()
+# y = boston.target
+
+y = data.quality(float)
+print(y)
+
 
 # cross_val_predict returns an array of the same size as `y` where each entry
-# is a prediction obtained by cross validation:
-predicted = cross_val_predict(lr, x, y, cv=10)
+# is a prediction obtained by cross validated:
+# predicted = cross_val_predict(lr, boston.data, y, cv=10)
 
-fig, ax = plt.subplots()
-ax.scatter(y, predicted)
-ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
-ax.set_xlabel('Temperature in C')
-ax.set_ylabel('Predicted')
-plt.show()
+# fig, ax = plt.subplots()
+# ax.scatter(y, predicted)
+# ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k-', lw=2)
+# ax.set_xlabel('Measured')
+# ax.set_ylabel('Predicted')
+# plt.show()
+
+
+
+
+# from sklearn import datasets, linear_model
+# from sklearn.model_selection import cross_val_predict
+# diabetes = datasets.load_diabetes()
+# X = diabetes.data[:150]
+# y = diabetes.target[:150]
+# lasso = linear_model.Lasso()
+# y_pred = cross_val_predict(lasso, X, y)
+
